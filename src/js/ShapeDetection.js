e8b3074864789588796fb74a7af77e2900a4e6ad
qdkmk <qdokuhon@gmail.com>
2019-10-21 18:03:57 +0900

    日本の書籍のisbnか否かを確認する機能追加

--- src/js/ShapeDetection.js
+++ src/js/ShapeDetection.js
@@ -36,48 +36,63 @@ class ShapeDetection {
 
   startBtnClick() {
     document.getElementById('rawValue').innerHTML = "";
-
-    navigator.mediaDevices.getUserMedia(this.medias).then((stream) => {
-        this.video.srcObject = stream;
-        this.localStream = stream;
-        if (window.BarcodeDetector) {
-          const detector = new BarcodeDetector();
-          detector.formats = "ean_13"
-          this.captureTimer = setInterval(() => {
-            detector.detect(this.video).then((barcodes) => {
-                let barcode = null;
-                for (barcode of barcodes) {
-                  //console.log(barcode);
-                  document.getElementById('rawValue').innerHTML = this.convertLink(barcode.rawValue);
-                  try {
-                    document.getElementById(env.LOADING).style.display = "block";
-                    GetApi.getgapi(barcode.rawValue).then((text) => {
-                        this.inputText.value += text;
-                        alert("書籍情報を自動入力しました")
-                      })
-                      .catch((err) => {
-                        console.log(err)
-                        alert("書籍情報の取得に失敗しました\n" + err)
-                      })
-                  } catch (err) {
-                    console.log(err);
-                  } finally {
-                    document.getElementById(env.LOADING).style.display = "none";
-                  }
-                }
-                if (barcode) this.stopVideo();
-              }).catch((err) => {
-              console.log(err);
-            });
-          }, 1000 / this.fps);
-        } else {
-          console.error('BarcodeDetection is not enable!');
+    navigator.mediaDevices.getUserMedia(this.medias).then(async(stream) => {
+      this.video.srcObject = stream;
+      this.localStream = stream;
+      this.switchButtoncondition();
+      if (window.BarcodeDetector) {
+        let result;
+        //正しいisbnを読み取れるまでスキャンし続ける
+        while(true){
+          result = await this.findIsbn();
+          if(/\d{13}/.test(result)){
+            this.callGetApi(result);
+            break;
+          }
         }
-        this.switchButtoncondition();
-      }).catch((err) => {
-        console.log(err);
-      });
+      } else {
+        console.error('BarcodeDetection is not enable!');
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
   }
+
+  findIsbn() {
+    return new Promise((resolve, reject) => {
+      const detector = new BarcodeDetector();
+      detector.formats = "ean_13"
+
+      this.captureTimer = setInterval(() => {
+        detector.detect(this.video).then((barcodes) => {
+          let barcode = null;
+          barcodes.some((barcode) => {
+            console.log(barcode.rawValue);
+            if (this.isValidISBN13(barcode.rawValue)) {
+              this.stopVideo();
+              return resolve(barcode.rawValue);
+            }else{
+              console.log("not isbn")
+            }
+          });
+        }).catch((err) => {
+          console.log(err);
+        });
+      }, 1000 / this.fps);
+    })
+  }
+  //Google Books APIの呼び出し
+  callGetApi(code) {
+    GetApi.getgapi(code).then((text) => {
+        this.inputText.value += text;
+        alert("書籍情報を自動入力しました")
+      })
+      .catch((err) => {
+        console.log(err)
+        alert("書籍情報の取得に失敗しました\n" + err)
+      })
+  }
+
 
   switchButtoncondition() {
     this.startBtn.disabled = !this.startBtn.disabled;
@@ -102,6 +117,26 @@ class ShapeDetection {
     }
     return str.replace(regexpUrl, regexpLink);
   };
+//isbnが9784からはじまることの確認（日本の書籍）とチェックデジットの検算
+  isValidISBN13(code) {
+    if (!code) return false;
+    if (13 != code.length) return false;
+    if(Number(code.charAt(0)) !== 9) return false;
+    if(Number(code.charAt(1)) !== 7) return false;
+    if(Number(code.charAt(2)) !== 8) return false;
+    if(Number(code.charAt(3)) !== 4) return false;
+    var sum = 0;
+    for (var i = 0; i < code.length; i++) {
+      var num = Number(code.charAt(i));
+
+      if (0 == (i % 2)) {
+        sum += num;
+      } else {
+        sum += (num * 3);
+      }
+    }
+    return (0 == (sum % 10));
+  }
 
 }
 
